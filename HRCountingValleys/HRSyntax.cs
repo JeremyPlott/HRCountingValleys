f1a111a958e1e4b52140b5601ddb4583b3592376
Jeremy <JeremyPlott@gmail.com>
2019-09-23 14:36:10 -0400

    Add project files.

---
+++ HRCountingValleys/HRSyntax.cs
@@ -0,0 +1,60 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, string s) {
+
+        var numList = new List<int>();
+        var steps = n;
+
+        foreach (char l in s) {
+            int value = 0;
+            if (l == 'D') { value = -1; }
+            if (l == 'U') { value = 1; }
+            numList.Add(value);
+        }
+
+        var elevation = new List<int>();
+        int valleyCounter = 0;
+        int idxcount = 0;
+        int elesum = 0;
+        foreach (int i in numList) {
+            elesum += i;
+            elevation.Add(elesum);
+
+            if (elesum == 0 && elevation[idxcount - 1] == -1) {
+                valleyCounter++;
+            }
+            idxcount++;
+        }
+        return valleyCounter;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string s = Console.ReadLine();
+
+        int result = countingValleys(n, s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ HRCountingValleys/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HRCountingValleys {
+    class Program {
+        static void Main(string[] args) {
+
+            int n = 8;
+            string s1 = "DDDUDUUU"; // 1 valley
+            string s2 = "UUDDDUDU"; // 2 valleys
+
+            // assign +1 -1 values to U / D
+            // tracker that sums each value
+            // whenever it goes from negative to 0, increment valley counter
+
+            var numList = new List<int>();
+
+            int AssignValue(char letter) {
+                int value = 0;
+                if (letter == 'D') { value = -1; }
+                if (letter == 'U') { value = 1;  }
+                return value;
+            }
+
+            foreach (char l in s2) {
+                numList.Add(AssignValue(l));
+            }            
+
+            var elevation = new List<int>();
+            int valleyCounter = 0;
+            int idxcount = 0;
+            int elesum = 0;
+            foreach(int i in numList) {
+                elesum += i;
+                elevation.Add(elesum);
+
+                if(elesum == 0 && elevation[idxcount - 1] == -1) {
+                    valleyCounter++;
+                }
+                Console.WriteLine($"Elevation {elevation[idxcount]}  valleys {valleyCounter}");
+                idxcount++;
+            }
+
+            // could just check for zeroes where previous int is negative and tally those
+        }
+    }
+}
